fe3eea7aa64f09c4dec085d81bd39af4749ef40e
Igor Brandão <igorbrandaocassimirodaflon@gmail.com>
2025-08-13 15:58:54 -0300

    Primeiro commit do projeto

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] arge)
+    {
+        float valorRecebido = 500.00f; 
+
+        bool doacaoAnonima = true; 
+
+        char tipoConta = 'P'; 
+
+        Console.WriteLine("Valor recebido: R$" + valorRecebido); 
+        Console.WriteLine("Doação anônima: " + doacaoAnonima); 
+        Console.WriteLine("Tipo de conta: " + tipoConta); 
+    }
+}
